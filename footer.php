e68d4148a089902ba840a721916c94a6d0337190
Shane Becker <sbecker@attineractive.com>
2010-08-04 11:34:52 -0700

    initial commit of toolbox wp starter theme

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Toolbox
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon">
+			<div id="site-generator">
+				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'themename' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'themename' ), 'WordPress' ); ?></a>
+			</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Toolbox
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content">
+
+			<?php if ( have_posts() ) : ?>
+
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'themename' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<?php get_template_part( 'loop', 'search' ); ?>
+
+			<?php else : ?>
+
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'themename' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'themename' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
