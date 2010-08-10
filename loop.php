2254684c1b59edcd4fa6513c1d0f70b285af1169
Shane Becker <sbecker@attineractive.com>
2010-08-10 15:53:02 -0700

    wired up gravatars to use author email correctly

--- loop.php
+++ loop.php
@@ -22,7 +22,7 @@
       <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'themename' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
       <div class="entry-meta">
-        <?php echo get_avatar( $id_or_email, '24' ); ?>
+        <?php echo get_avatar( get_the_author_meta('user_email'), '24' ); ?>
         <?php
           printf( __( '<span class="meta-prep meta-prep-author">Posted on </span><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a> <span class="meta-sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s">%6$s</a></span>', 'themename' ),
             get_permalink(),
